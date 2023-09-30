ebda5a84c68b183066a9a92a6de69b50a78df00f
Nikul Khatik <nikul@trustedcommunities.org>
2023-09-30 18:08:09 +0530

    Laravel auth added and roles & permission configured

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Traits\HasPermissionsTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
     use HasFactory;
-    use HasProfilePhoto;
     use Notifiable;
-    use TwoFactorAuthenticatable;
+    use HasPermissionsTrait;
 
     /**
      * The attributes that are mass assignable.
